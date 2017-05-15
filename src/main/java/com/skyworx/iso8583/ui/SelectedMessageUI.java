7f36ec641d8807293d0bf0afa6a03f57f147acba
Faisal Abdillah <faitz1989@gmail.com>
2017-05-16 05:34:12 +0700

    separate edit view

---
+++ src/main/java/com/skyworx/iso8583/ui/SelectedMessageUI.java
@@ -0,0 +1,24 @@
+package com.skyworx.iso8583.ui;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by faitz on 5/16/2017.
+ */
+public class SelectedMessageUI {
+
+    public TextField selectedMessageName;
+    public TableView messageEditTable;
+    public TextField bitField;
+
+
+    public void saveSelectedBitMessage(ActionEvent actionEvent) {
+
+    }
+
+    public void clearSelectedBitMessage(ActionEvent actionEvent) {
+
+    }
+}
