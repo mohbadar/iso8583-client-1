2f805bfc793b110c8c86f674971843a1fccc58c4
Faisal Abdillah <faitz1989@gmail.com>
2017-05-22 22:52:42 +0700

    add packager

---
+++ src/main/java/com/skyworx/iso8583/Launcher.java
@@ -0,0 +1,11 @@
+package com.skyworx.iso8583;
+
+import javafx.application.Application;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        MainApplication.launch(MainApplication.class, args);
+    }
+
+}
